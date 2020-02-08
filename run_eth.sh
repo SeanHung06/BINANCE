1c2650bd32b58eca7f7f2449e4573d22444c7389
seanhung06 <u8351574@gmail.com>
2020-02-08 18:04:54 +0800

    1

--- run_eth.sh
+++ run_eth.sh
@@ -1,7 +1,7 @@
 # 記錄一下開始時間
-echo `date` >> /Users/seanhung06/Desktop/python/Binance/log &&
+echo `date` >> /Users/seanhung06/Binance/log &&
 # 進入helloworld.py程式所在目錄
-cd /Users/seanhung06/Desktop/python/Binance &&
+cd /Users/seanhung06/Binance &&
 # 執行python指令碼（注意前面要指定python執行環境/usr/bin/python，根據自己的情況改變）
 /usr/local/bin/python3 binance1.py
 /usr/local/bin/python3 email_send.py
