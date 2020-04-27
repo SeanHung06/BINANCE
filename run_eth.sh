4d3cd2cbbd68951e7d59378214846cec36ff8a82
Sean Hong <u8351574@gmail.com>
2020-04-27 21:41:05 +0800

    Windows version

--- run_eth.sh
+++ run_eth.sh
@@ -1,9 +1,9 @@
 # 記錄一下開始時間
-echo `date` >> /Users/seanhung06/Binance/log &&
+echo `date` >> /c/Users/USER/Desktop/python/BINANCE/log &&
 # 進入helloworld.py程式所在目錄
-cd /Users/seanhung06/Binance &&
+cd /c/Users/USER/Desktop/python/BINANCE &&
 # 執行python指令碼（注意前面要指定python執行環境/usr/bin/python，根據自己的情況改變）
 #/usr/local/bin/python3 binance1.py
-/usr/local/bin/python3 ./MACD/get_Binance.py
-/usr/local/bin/python3 email_send.py
+./MACD/get_Binance.py
+email_send.py
 # 執行完成
--- run_eth_notify.sh
+++ run_eth_notify.sh
@@ -1,9 +1,9 @@
 # 記錄一下開始時間
-echo `date` >> /Users/seanhung06/Binance/log_notify &&
+echo `date` >> /c/Users/USER/Desktop/python/BINANCE/log_notify &&
 # 進入helloworld.py程式所在目錄
-cd /Users/seanhung06/Binance &&
+cd /c/Users/USER/Desktop/python/BINANCE &&
 # 執行python指令碼（注意前面要指定python執行環境/usr/bin/python，根據自己的情況改變）
-/usr/local/bin/python3 binance1.py
-/usr/local/bin/python3 ./MACD/get_Binance.py
-/usr/local/bin/python3 email_price_notify.py
+binance1.py
+./MACD/get_Binance.py
+email_price_notify.py
 # 執行完成
