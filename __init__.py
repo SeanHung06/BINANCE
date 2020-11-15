8555aa46be372a560f3b7189044c2b2eb99239c3
seanhung06 <u8351574@gmail.com>
2020-11-15 15:46:54 +0800

    Update the Logs Parameter

---
+++ __init__.py
@@ -0,0 +1,3 @@
+# 從套件中引用模組
+from blog import post
+from blog import about
--- run_eth.sh
+++ run_eth.sh
@@ -1,5 +1,5 @@
 # 記錄一下開始時間
-echo `date` >> /Users/seanhung06/Binance/log &&
+echo `date` >> /Users/seanhung06/Binance/Logs/log &&
 # 進入helloworld.py程式所在目錄
 cd /Users/seanhung06/Binance &&
 # 執行python指令碼（注意前面要指定python執行環境/usr/bin/python，根據自己的情況改變）
